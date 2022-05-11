732df54106c1aa2f55d8463415cba01f4a329581
I'm gonna be a cat girl <77449641+Sagiri721@users.noreply.github.com>
2022-05-11 17:18:12 +0100

    Normalização

--- createTables.sql
+++ createTables.sql
@@ -3,14 +3,13 @@ CREATE TABLE IF NOT EXISTS User (
     nome_da_conta TEXT,
     seguidores INTEGER,
     descricao TEXT,
-    localizacao TEXT,
+    pais TEXT,
+    loc_detalhes TEXT,
     data_nascimento TEXT
 );
-CREATE TABLE IF NOT EXISTS TipoPost (descricao TEXT);
+CREATE TABLE IF NOT EXISTS TipoPost (descricao TEXT, cor_do_texto TEXT);
 CREATE TABLE IF NOT EXISTS Post (
     autor INTEGER texto TEXT,
     texto TEXT,
-    tipo_de_post INTEGER,
-    cor_do_texto TEXT,
-    tamanho INTEGER
+    tipo_de_post INTEGER
 );
--- database.py
+++ database.py
@@ -1,8 +1,12 @@
-from re import S
 import sqlite3
+import os
 
 
 def StartDatabase():
+
+    # Remove if already exists
+    if os.path.exists("bdExemplo.db"):
+        os.remove("bdExemplo.db")
 
     conn = sqlite3.connect("bdExemplo.db")
     cursor = conn.cursor()
@@ -38,3 +42,38 @@ def FazerUmPost(recordList):
 
     conn.commit()
     cursor.close()
+
+
+def FetchUsers():
+    conn = sqlite3.connect("bdExemplo.db")
+    cursor = conn.cursor()
+
+    sqlQuerry = open("GetTableResults\getAllUsers.sql").read()
+
+    cursor.execute(sqlQuerry)
+
+    return cursor.fetchall()
+
+
+def FetchPosts():
+    conn = sqlite3.connect("bdExemplo.db")
+    cursor = conn.cursor()
+
+    sqlQuerry = open("GetTableResults\getAllPosts.sql").read()
+
+    cursor.execute(sqlQuerry)
+
+    return cursor.fetchall()
+
+# StartDatabase()
+# AdicionarUtilizador(["Sagiri721_", "變態猫「紗霧」", 24,
+#                    "Tiago Simões, desenvolvedor de software",
+#                     "Porto", "Portugal", "21/7/2005"])
+#FazerUmPost([0, "Olá Mundo!", 0])
+
+
+print("-----------------------------------------\nUsers")
+print(FetchUsers())
+
+print("-----------------------------------------\nPosts")
+print(FetchPosts())
